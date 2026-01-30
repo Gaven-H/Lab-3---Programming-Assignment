4744a424be802c67f85c073b017f7833d93f731c
Bemst <gahuforschool@gmail.com>
2026-01-29 23:41:47 -0500

    Add fix

--- Lab3_ghuynh_add.py
+++ Lab3_ghuynh_add.py
@@ -7,6 +7,6 @@ n/a
 """
 from Lab3_ghuynh_list import items_list
 
-items_list.extend(["Rope, Sunscreen, Heater, Hat, Camera"])
+items_list.extend(["Rope", "Sunscreen", "Heater", "Hat", "Camera"])
 
 print ("Reverse alphabetical list (plus additional items):", sorted(items_list, reverse=True))
