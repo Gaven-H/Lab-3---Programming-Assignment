5a30a953306495b466d587909563f3e86e0a0817
Bemst <gahuforschool@gmail.com>
2026-01-29 23:01:59 -0500

    docstring + items_list list

---
+++ Lab3_ghuynh_list.py
@@ -0,0 +1,9 @@
+"""
+Camping Items
+Gaven Huynh
+List of Camping Items
+n/a
+1/29/2026
+"""
+
+items_list = ["Tent", "Fire Starter", "Bed Roll", "Light Sources", "Foraging Guide", "Food Supplies", "Water", "Tools", "Radio", "First Aid Kit"]
