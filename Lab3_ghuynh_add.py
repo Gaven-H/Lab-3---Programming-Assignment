b78722e11ccf65b34b838449066be21c285f59a1
Bemst <gahuforschool@gmail.com>
2026-01-29 23:20:26 -0500

    Reverse alphabetical print

--- Lab3_ghuynh_add.py
+++ Lab3_ghuynh_add.py
@@ -9,3 +9,4 @@ from Lab3_ghuynh_list import items_list
 
 items_list.extend(["Rope, Sunscreen, Heater, Hat, Camera"])
 
+print ("Reverse alphabetical list (plus additional items):", sorted(items_list, reverse=True))
